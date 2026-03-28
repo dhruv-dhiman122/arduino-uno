6d440b57a0a5d4ffee63e68231ba8f6e8b2850d7
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-03-28 18:46:23 +0530

    We have defined the pins for the analog reading from sensors

---
+++ main.ino
@@ -0,0 +1,21 @@
+
+
+// ============== space for macro or golbal defined variables =============
+
+#define MQ5_1 A0
+#define MQ5_2 A1
+#define MQ9_1 A2
+#define MQ9_2 A3
+#define MQ135_1 A4
+#define MQ135_2 A5
+
+// ========== Space for only setup function ==========
+
+void setup() {
+    Serial.begin(9600);
+}
+
+// ========= Space for only loop function =============
+void loop() {
+
+}
